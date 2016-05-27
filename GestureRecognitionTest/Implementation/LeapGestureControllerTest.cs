044a7a8bdebb5717e67246003d11d5469d2afbbf
Christian Blank <christian@colorful-projects.de>
2016-05-27 11:31:59 +0200

    Fix length of bones issue and remove intermediate bone of thumb from data (#4)

--- GestureRecognitionTest/Implementation/LeapGestureControllerTest.cs
+++ GestureRecognitionTest/Implementation/LeapGestureControllerTest.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading;
+﻿using System.Linq;
+using System.Threading;
 using GestureRecognition.Implementation;
 using GestureRecognition.Interface;
 using GestureRecognition.Interface.Commands;
@@ -26,6 +27,22 @@ namespace GestureRecognitionTest
             commandWaiter.Wait(5000);
 
             Assert.IsTrue(commandReceived);
+        }
+
+        [Test]
+        [Ignore("This tests needs a connected Leap Motion. It will be ignored in continous integration")]
+        public void TestThumbsHasNoIntermediate()
+        {
+            var hasIntermediate = false;
+            var commandWaiter = new ManualResetEventSlim();
+            var gestureRecognition = GestureRecognitionFactory.Create(new LeapGestureController());
+            gestureRecognition.SubscribeToCommand<PhysicCommand>(x =>
+            {
+                hasIntermediate = x.BodyParts.Any(part => part.Id == 20613 || part.Id == 22613);
+                commandWaiter.Set();
+            });
+            commandWaiter.Wait(5000);
+            Assert.IsFalse(hasIntermediate, "Thumbs should not have an intermediate bone");
         }
     }
 }
