8f3b7521c399db018f75c5b486a8badec83acc8a
Malte Eckhoff <malte.eckhoff@gmail.com>
2016-07-13 20:41:41 +0200

    LeapPlayer loop functionality

--- GestureRecognition/Implementation/LeapGestureController/LeapPlayer.cs
+++ GestureRecognition/Implementation/LeapGestureController/LeapPlayer.cs
@@ -1,60 +1,128 @@
-﻿using System;
-using System.IO;
-using System.Threading;
-using GestureRecognition.Implementation.Serializer;
-using GestureRecognition.Interface;
-using Leap;
-
-namespace GestureRecognition.Implementation
+﻿namespace GestureRecognition.Implementation
 {
+    using System;
+    using System.Collections.Concurrent;
+    using System.IO;
+    using System.Threading;
+
+    using global::GestureRecognition.Implementation.Serializer;
+    using global::GestureRecognition.Interface;
+
+    using Leap;
 
     public class LeapPlayer : ILeapController
     {
-        private string _path;
-        private Thread _thread;
+        private const int NextFrameSizeLength = 4;
+
+        /// <summary>
+        /// True if the output will be looped after reaching the end of the file, otherwise false.
+        /// </summary>
+        public bool LoopOutput = false;
+
         private CancellationTokenSource _cancellationTokenSource;
+
+        private string _path;
+
+        private System.Threading.Tasks.Task _producerTask;
+
+        private System.Threading.Tasks.Task _consumerTask;
+
+        private long numberOfFrames = -1;
+
+        private ConcurrentQueue<Frame> readyFrames = new ConcurrentQueue<Frame>();
 
         public LeapPlayer(string path)
         {
-            _cancellationTokenSource = new CancellationTokenSource();
-            _path = path;
-            _thread = new Thread(Run);
+            this._cancellationTokenSource = new CancellationTokenSource();
+            this._path = path;
+            
+            this._producerTask = new System.Threading.Tasks.Task(this.FillFrameBuffer, this._cancellationTokenSource.Token);
+            this._consumerTask = new System.Threading.Tasks.Task(this.ConsumeFrameBuffer, this._cancellationTokenSource.Token);
+        }
+
+        public long TotalBytesRead { get; private set; } = 0;
+
+        public event Action<Frame> FrameReady;
+
+        /// <summary>
+        /// Gets the number of bytes available in the underlying file of this player.
+        /// </summary>
+        public long FileSizeInBytes
+        {
+            get
+            {
+                if (this.numberOfFrames == -1)
+                {
+                    FileInfo loopbackFileInfo = new FileInfo(this._path);
+                    this.numberOfFrames = loopbackFileInfo.Length;
+                }
+
+                return this.numberOfFrames;
+            }
+        }
+
+        public void StartConnection()
+        {
+            this._producerTask.Start();
+            this._consumerTask.Start();
         }
 
         public void StopConnection()
         {
-            _cancellationTokenSource.Cancel();
+            this._cancellationTokenSource.Cancel();
         }
 
-        public event Action<Frame> FrameReady;
-
-        public void StartConnection()
+        private void ConsumeFrameBuffer()
         {
-            _thread.Start();
+            while (!this._cancellationTokenSource.Token.IsCancellationRequested)
+            {
+                Frame nextFrame;
+                bool frameSuccessfullyDequeued = this.readyFrames.TryDequeue(out nextFrame);
+
+                if (frameSuccessfullyDequeued)
+                {
+                    this.FireFrameReady(nextFrame);
+                }
+            }
         }
 
-        private void Run()
+        private void FillFrameBuffer()
         {
             // load file
-            var sizeBytes = new byte[4];
+            var sizeBytes = new byte[NextFrameSizeLength];
             var serializer = new LeapFrameSerializer();
-            using (var file = File.Open(_path, FileMode.Open))
+            using (var file = File.Open(this._path, FileMode.Open))
             {
-                while (!_cancellationTokenSource.Token.IsCancellationRequested)
+                while (!this._cancellationTokenSource.Token.IsCancellationRequested)
                 {
-                    file.Read(sizeBytes, 0, 4);
-                    var length = BitConverter.ToInt32(sizeBytes, 0);
-                    var objBuffer = new byte[length];
-                    file.Read(objBuffer, 0, length);
-                    var frame = serializer.Deserialize(objBuffer);
-                    FireFrameReady(frame);
+                    // Read how long the next frame is.
+                    var bytesRead = file.Read(sizeBytes, 0, NextFrameSizeLength);
+                    this.TotalBytesRead += bytesRead;
+
+                    // If the bytes read are less than 4 bytes, the end of the file has been reached. Otherwise we have more data to read.
+                    if (bytesRead == NextFrameSizeLength)
+                    {
+                        // Process the playback data read from the file.
+                        var length = BitConverter.ToInt32(sizeBytes, 0);
+                        this.TotalBytesRead += length;
+                        var objBuffer = new byte[length];
+                        file.Read(objBuffer, 0, length);
+                        var frame = serializer.Deserialize(objBuffer);
+
+                        this.readyFrames.Enqueue(frame);
+                    }
+                    else if (this.LoopOutput)
+                    {
+                        // If the output should loop, reset the stream when the end of the file is reached.
+                        file.Seek(0, SeekOrigin.Begin);
+                    }
                 }
             }
         }
 
         private void FireFrameReady(Frame f)
         {
-            FrameReady?.Invoke(f);
+            this.FrameReady?.Invoke(f);
         }
     }
 }
