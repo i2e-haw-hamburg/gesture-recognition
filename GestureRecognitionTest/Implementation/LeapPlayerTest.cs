7f761372c351e9318f8f3e492665a476893b8e18
Christian Blank <christian@colorful-projects.de>
2016-07-15 08:40:43 +0200

    LeapPlayer loop functionality (#6)

---
+++ GestureRecognitionTest/Implementation/LeapPlayerTest.cs
@@ -0,0 +1,52 @@
+﻿namespace GestureRecognitionTest.Implementation
+{
+    using System;
+    using System.Threading;
+
+    using GestureRecognition.Implementation;
+
+    using NUnit.Framework;
+
+    using Timer = System.Timers.Timer;
+
+    [TestFixture]
+    public class LeapPlayerTest
+    {
+        /// <summary>
+        /// Tests if the LeapPlayers data is looped when the end of the underlying file is reached.
+        /// </summary>
+        [Test]
+        public void TestLeapPlayerLoop()
+        {
+            var testOver = new ManualResetEventSlim();
+            var timeoutTimer = new Timer(100);
+
+            var player = new LeapPlayer(@"frames\rotate_1.frames");
+            player.LoopOutput = true;
+
+            var bytesExpected = player.FileSizeInBytes * 3;
+            
+            Action<Leap.Frame> frameListener = frame =>
+            {
+                timeoutTimer.Stop();
+                timeoutTimer.Start();
+                if (player.TotalBytesRead >= bytesExpected)
+                {
+                    testOver.Set();
+                }
+            };
+            player.FrameReady += frameListener;
+
+            timeoutTimer.Elapsed += (sender, args) => testOver.Set(); 
+            
+            timeoutTimer.Start();
+            player.StartConnection();
+
+            testOver.Wait();
+
+            player.StopConnection();
+
+            Assert.IsTrue(player.TotalBytesRead >= bytesExpected);
+        }
+    }
+}
